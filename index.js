8173f6708eb7ec1a6e9a1a8423b638d175002d96
Asif Ferdous <asifferdous23@gmail.com>
2022-11-10 00:26:18 +0600

    dotenv

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const user="asif";
-const password="asif1234";
 // middle wares
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${user}:${password}@cluster0.bs7nnrw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.bs7nnrw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
