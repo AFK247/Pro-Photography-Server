117d7adb9bbc61c5bf46716544355da4dcf0ec2a
Asif Ferdous <asifferdous23@gmail.com>
2022-11-10 15:16:31 +0600

    readme

---
+++ README.md
@@ -0,0 +1,36 @@
+Project Name: Pro Photography
+
+
+This is Server side 
+
+The Github Repository Link for Client:  https://github.com/Porgramming-Hero-web-course/b6a11-service-review-client-side-AFK247
+
+The Github Repository Link for Server: https://github.com/Porgramming-Hero-web-course/b6a11-service-review-server-side-AFK247
+
+Live Website Link: https://photography-2351.web.app
+
+Project Work: 
+
+* In this project I created some Services of different Photography Skills
+
+* I used React router to route through different components
+
+* I used context API
+
+* I used Responsive design in the website
+
+* I used Private Route in the website
+
+* I used Mongodb For Database
+
+Framework/Libraries Used:
+
+* CORS
+
+* React-Router-Dom
+
+* React-Toast
+
+* Dotenv
+
+* Bootstrap
--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const user=process.env.db_user;
-const password=process.env.db_password;
+const user = process.env.db_user;
+const password = process.env.db_password;
 
 //The user and password were taken from env
 const uri = `mongodb+srv://${user}:${password}@cluster0.bs7nnrw.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,11 +23,11 @@ async function run() {
     try {
 
         const serviceCollection = client.db('photo').collection('services');
-        const reviewCollection = client.db('photo').collection('review'); 
+        const reviewCollection = client.db('photo').collection('review');
 
         app.get('/services3', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query).sort({_id:-1}).limit(3);
+            const cursor = serviceCollection.find(query).sort({ _id: -1 }).limit(3);
             const limited = await cursor.toArray();
             res.send(limited);
         });
@@ -55,7 +55,7 @@ async function run() {
         app.get('/review/:id', async (req, res) => {
             const id = req.params.id;
             const query = { service: id };
-            const cursor =reviewCollection.find(query);
+            const cursor = reviewCollection.find(query);
             const review = await cursor.toArray();
             res.send(review);
         });
@@ -63,27 +63,33 @@ async function run() {
         app.get('/myreview/:id', async (req, res) => {
             const id = req.params.id;
             const query = { customer_id: id };
-            const cursor =reviewCollection.find(query);
+            const cursor = reviewCollection.find(query);
             const review = await cursor.toArray();
             res.send(review);
         });
 
         app.delete('/delete/:id', async (req, res) => {
-                const id = req.params.id;
-                const query = { _id: ObjectId(id) };
-                const result = await reviewCollection.deleteOne(query);
-                res.send(result);
-            })
-            app.post('/addservice', async (req, res) => {
-                const review = req.body;
-                const result = await serviceCollection.insertOne(review);
-                res.send(result);
-            });
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.post('/addservice', async (req, res) => {
+            const review = req.body;
+            const result = await serviceCollection.insertOne(review);
+            res.send(result);
+        });
+
+        // app.post('/updatereview', async (req, res) => {
+        //     const review = req.body;
+
+        
+        // });
 
 
     }
     finally {
-
+        //Nothing
     }
 
 }
@@ -92,7 +98,7 @@ run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
-    res.send('Photo server is running')
+    res.send('Photo server is running Succesfully')
 })
 
 app.listen(port, () => {
