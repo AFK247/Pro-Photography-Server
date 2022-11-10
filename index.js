b447bf2547ac95ef517e1c5142300f2cbb94a414
Asif Ferdous <asifferdous23@gmail.com>
2022-11-10 12:02:02 +0600

    update

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//The user and password were taken from env
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.bs7nnrw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+//Two collection is used
 async function run() {
     try {
         const serviceCollection = client.db('photo').collection('services');
